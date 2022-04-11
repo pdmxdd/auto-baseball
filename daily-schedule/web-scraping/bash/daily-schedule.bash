7da8c20b3f420716386740ce1cb4d686cba9f6e4
Paul Matthews <paul@paulmatthews.dev>
2022-04-10 22:13:55 -0500

    original web-scraping script

---
+++ daily-schedule/web-scraping/bash/daily-schedule.bash
@@ -0,0 +1,1 @@
+curl -s https://www.baseball-reference.com/  | grep -zo "<h3>Today's Games.*<h3>" | sed 's/(S)//g' | sed 's/<\/*[^>]*>//g' | sed '/^[[:space:]]*$/d' | awk '{$1=$1;print}' | sed -e '2,${N;N;N;s/\n/ /g}' > bb-ref.schedule
